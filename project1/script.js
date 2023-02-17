97851c5dfd29d56b8f6098040947da81193a8b52
Rashid Khamis <116562712+Rashidkhamis9680@users.noreply.github.com>
2023-02-17 22:43:28 +0300

    counter project completed

--- project1/script.js
+++ project1/script.js
@@ -2,15 +2,34 @@
 
  let btns = document.querySelectorAll(".material-symbols-outlined");
  let value = document.querySelector("#num");
+ let colorOne = document.querySelector('.changer');
 
   btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
-      let button = e.currentTarget.classList; 
+      let button = e.currentTarget.classList;
+      console.log(button) 
       if(button.contains("positive")){
       count ++
+       
      }else if(button.contains("negative")){
        count--
-     } 
+        
+
+     } else{
+      count = 0;
+      
+     }
      value.textContent = count;
-    })
+     
+     if(count > 0){
+      colorOne.style.backgroundColor = "green";
+      colorOne.style.color = "white";
+     }else if(count < 0){
+      colorOne.style.backgroundColor = "firebrick";
+      colorOne.style.color = "white";
+         }else{
+          colorOne.style.backgroundColor = "white";
+          colorOne.style.color = "black";
+         }
   })
+  });
